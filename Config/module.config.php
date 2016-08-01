86d4c0fbd6ed2306d8e872c47fe7ebb669909ce2
David Yang <daworld.ny@gmail.com>
2016-08-01 22:27:24 +0500

    Removed extras from module configuration file

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,11 +1,9 @@
 <?php
 
 return array(
-    'module' => array(
-        'name' => 'Blog',
-        'route' => 'Blog:Admin:Browser@indexAction',
-        'icon' => 'fa fa-commenting fa-5x',
-        'order' => 10,
-        'description' => 'Blog module allows you to create a personal blog on your site',
-    )
+ 'name' => 'Blog',
+ 'route' => 'Blog:Admin:Browser@indexAction',
+ 'icon' => 'fa fa-commenting fa-5x',
+ 'order' => 10,
+ 'description' => 'Blog module allows you to create a personal blog on your site',
 );
