1e795cba72e1a8c327984d786a54ca07d700d398
dave-yang <daworld.ny@gmail.com>
2019-03-07 20:16:43 +0500

    Update module.config.php

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,9 +1,31 @@
 <?php
 
+/**
+ * Module configuration container
+ */
+
 return array(
- 'name' => 'Blog',
- 'caption' => 'Blog',
- 'route' => 'Blog:Admin:Browser@indexAction',
- 'order' => 10,
- 'description' => 'Blog module allows you to create a personal blog on your site',
+    'description' => 'Blog module allows you to create a personal blog on your site',
+    'menu' => array(
+        'name' => 'Blog',
+        'icon' => 'fas fa-box-open',
+        'items' => array(
+            array(
+                'route' => 'Blog:Admin:Browser@indexAction',
+                'name' => 'View all posts'
+            ),
+            array(
+                'route' => 'Blog:Admin:Post@addAction',
+                'name' => 'Add a post'
+            ),
+            array(
+                'route' => 'Blog:Admin:Category@addAction',
+                'name' => 'Add category'
+            ),
+            array(
+                'route' => 'Blog:Admin:Config@indexAction',
+                'name' => 'Configuration'
+            )
+        )
+    )
 );
