78ac194cee2f04b3e43e53a827c90aca3f31b7e3
dave-yang <daworld.ny@gmail.com>
2019-08-24 17:05:08 +0500

    Fixes in schema

--- Storage/MySQL/schema.sql
+++ Storage/MySQL/schema.sql
@@ -1,18 +1,16 @@
 
+/* Blog categories */
 DROP TABLE IF EXISTS `bono_module_blog_categories`;
 CREATE TABLE `bono_module_blog_categories` (
-
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `parent_id` INT NOT NULL COMMENT 'Parent category id',
 	`seo` varchar(1) NOT NULL COMMENT 'Whether SEO enabled or not',
 	`order` INT NOT NULL COMMENT 'Sort order',
     `cover` varchar(50) NOT NULL COMMENT 'Image file basename'
-
-) DEFAULT CHARSET = UTF8;
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
 
 DROP TABLE IF EXISTS `bono_module_blog_categories_translations`;
 CREATE TABLE `bono_module_blog_categories_translations` (
-
     `id` INT NOT NULL,
 	`lang_id` INT NOT NULL COMMENT 'Language id this category belongs to',
 	`web_page_id` INT NOT NULL COMMENT 'Web page id this category belongs to',
@@ -20,14 +18,16 @@ CREATE TABLE `bono_module_blog_categories_translations` (
 	`name` varchar(255) NOT NULL COMMENT 'Name of the category',
 	`description` LONGTEXT NOT NULL COMMENT 'Description of the category',
 	`keywords` TEXT NOT NULL COMMENT 'Meta keywords for SEO',
-	`meta_description` TEXT NOT NULL COMMENT 'Meta description for SEO'
+	`meta_description` TEXT NOT NULL COMMENT 'Meta description for SEO',
 
-) DEFAULT CHARSET = UTF8;
+    FOREIGN KEY (id) REFERENCES bono_module_blog_categories(id) ON DELETE CASCADE,
+    FOREIGN KEY (lang_id) REFERENCES bono_module_cms_languages(id) ON DELETE CASCADE,
+    FOREIGN KEY (web_page_id) REFERENCES bono_module_cms_webpages(id) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
 
-
+/* Blog posts */
 DROP TABLE IF EXISTS `bono_module_blog_posts`;
 CREATE TABLE `bono_module_blog_posts` (
-
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `category_id` INT NOT NULL,
     `timestamp` INT(10) NOT NULL,
@@ -35,14 +35,13 @@ CREATE TABLE `bono_module_blog_posts` (
     `comments` varchar(1) NOT NULL,
     `seo` varchar(1) NOT NULL,
     `cover` varchar(255),
-    `views` INT NOT NULL
+    `views` INT NOT NULL,
 
-) DEFAULT CHARSET = UTF8;
-
+    FOREIGN KEY (category_id) REFERENCES bono_module_blog_categories(id) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
 
 DROP TABLE IF EXISTS `bono_module_blog_posts_translations`;
 CREATE TABLE `bono_module_blog_posts_translations` (
-
     `id` INT NOT NULL,
     `lang_id` INT NOT NULL,
     `web_page_id` INT NOT NULL,
@@ -51,12 +50,18 @@ CREATE TABLE `bono_module_blog_posts_translations` (
     `introduction` LONGTEXT NOT NULL,
     `full` LONGTEXT NOT NULL,
     `keywords` TEXT NOT NULL,
-    `meta_description` TEXT NOT NULL
+    `meta_description` TEXT NOT NULL,
 
-) DEFAULT CHARSET = UTF8;
+    FOREIGN KEY (id) REFERENCES bono_module_blog_posts(id) ON DELETE CASCADE,
+    FOREIGN KEY (lang_id) REFERENCES bono_module_cms_languages(id) ON DELETE CASCADE,
+    FOREIGN KEY (web_page_id) REFERENCES bono_module_cms_webpages(id) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
 
+/* Post attachments */
 DROP TABLE IF EXISTS `bono_module_blog_posts_attached`;
 CREATE TABLE `bono_module_blog_posts_attached` (
-    `master_id` INT NOT NULL COMMENT 'Post ID',
-    `slave_id` INT NOT NULL COMMENT 'Attached post ID'
-) DEFAULT CHARSET = UTF8;
+    `master_id` INT NOT NULL COMMENT 'Primary Post ID',
+    `slave_id` INT NOT NULL COMMENT 'Attached post ID',
+
+    FOREIGN KEY (id) REFERENCES master_id(id) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
